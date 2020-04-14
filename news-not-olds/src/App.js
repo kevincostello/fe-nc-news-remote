782e776d7500ef046af85bd2ff45e0a64b73ee51
Kevin Costello <kevin_m_costello@hotmail.com>
2020-04-14 12:18:04 +0100

    Created header and global.css

--- news-not-olds/src/App.js
+++ news-not-olds/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./styles/components/Header";
+import "./styles/global.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
